a623303c5c9c4a82ab4e309f13e94bf431b3a4a6
jedaitemple <kosio972@mail.bg>
2016-02-02 23:51:42 +0000

    d
    d

---
+++ Begining.java
@@ -0,0 +1,36 @@
+package com.chatt.kosio;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+/**
+ * The main class connection to Parse
+ */
+public class Begining extends Application
+{
+
+	/* (non-Javadoc)
+	 * @see android.app.Application#onCreate()
+	 */
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+
+		Parse.initialize(this, "nE2rM3YtEq45TU6ktVBTzekMIFilQzg5c693IueV",
+				"v3NTJ6khp6X2q7uqU7fNhFMsWfMiMnuWjuigz6KQ");
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+
+
+		defaultACL.setPublicReadAccess(true);
+
+		Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+
+
+	}
+}
