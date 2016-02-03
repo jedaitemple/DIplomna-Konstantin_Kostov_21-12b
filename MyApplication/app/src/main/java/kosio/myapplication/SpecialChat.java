943a66664474512e69d6537af2f259b6327a06e5
jedaitemple <kosio972@mail.bg>
2016-02-03 15:06:03 +0000

    Create SpecialChat.java

---
+++ MyApplication/app/src/main/java/kosio/myapplication/SpecialChat.java
@@ -0,0 +1,669 @@
+package  kosio.myapplication;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
+import android.text.format.DateUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.io.ByteArrayOutputStream;
+import com.parse.ParseImageView;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.graphics.Bitmap;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import com.chatt.demo.custom.CustomActivity;
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+import android.widget.ImageView;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+import com.chatt.demo.custom.CustomActivity;
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kosioplay on 30.1.2016 г..
+ */
+import android.app.Activity;
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.os.Handler;
+
+import android.os.SystemClock;
+
+import android.view.View;
+
+import android.widget.Button;
+
+import android.widget.TextView;
+
+
+import android.os.SystemClock;
+
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
+import android.text.format.DateUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.io.ByteArrayOutputStream;
+
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.graphics.Bitmap;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import com.chatt.demo.custom.CustomActivity;
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+import android.widget.ImageView;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.text.InputType;
+import android.text.format.DateUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SpecialChat extends CustomActivity {
+
+    /**
+     * The Conversation list.
+     */
+    private ArrayList<Conversation> convList;
+
+    /**
+     * The chat adapter.
+     */
+    private ChatAdapter adp;
+
+    /**
+     * The Editext to compose the message.
+     */
+    private Button startButton;
+    private Button pauseButton;
+    private TextView timerValue;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+    Bitmap bmp = null;
+    /**
+     * The Conversation list.
+     */
+
+    /**
+     * The chat adapter.
+     */
+
+    /**
+     * The Editext to compose the message.
+     */
+    private EditText txt;
+    private EditText txt1;
+    /**
+     * The user name of buddy.
+     */
+    private String buddy;
+
+    /**
+     * The date of last message in conversation.
+     */
+    private Date lastMsgDate;
+
+    /**
+     * Flag to hold if the activity is running or not.
+     */
+    private boolean isRunning;
+
+    /**
+     * The handler.
+     */
+    /**
+     * The handler.
+     */
+    private PendingIntent pendingIntent;
+    private static Handler handler;
+    private static int RESULT_LOAD_IMAGE = 1;
+    Button button;
+    ImageView viewImage;
+    Button b;
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+	 */
+    EditText eText;
+    Button btn;
+    // GUI components
+
+    protected void onCreate(Bundle savedInstanceState) {  // Image loading result to pass to startActivityForResult method.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.specialchat);
+        deleteStudent();
+        convList = new ArrayList<Conversation>();
+        ListView list = (ListView) findViewById(R.id.list);
+        adp = new ChatAdapter();
+        list.setAdapter(adp);
+        list.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        list.setStackFromBottom(true);
+
+        txt = (EditText) findViewById(R.id.txt);
+        txt.setInputType(InputType.TYPE_CLASS_TEXT
+                | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+
+        eText = (EditText) findViewById(R.id.edittext);
+        btn = (Button) findViewById(R.id.btnuser);
+        btn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                String str = eText.getText().toString();
+                buddy = str;
+                Toast msg = Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
+
+        setTouchNClick(R.id.btnSend);
+
+
+        Button buttonLoadImage = (Button) findViewById(R.id.btnSelectPhoto);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+
+
+
+        handler = new Handler();
+    }
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.FragmentActivity#onResume()
+     */
+
+    public void deleteStudent(){
+        ParseQuery<ParseObject> query=ParseQuery.getQuery("Chat");
+        query.whereEqualTo("special", "special");
+        query.whereEqualTo("receiver", UserList.user.getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if(e==null) {
+
+
+                    for (ParseObject delete : parseObjects) {
+                        delete.deleteInBackground();
+                        Toast.makeText(getApplicationContext(), "deleted", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "error in deleting", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
+
+    protected void onResume()
+    {
+        super.onResume();
+        isRunning = true;
+        loadConversationList();
+    }
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.FragmentActivity#onPause()
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        isRunning = false;
+    }
+
+    /* (non-Javadoc)
+     * @see com.socialshare.custom.CustomFragment#onClick(android.view.View)
+     */
+    @Override
+    public void onClick(View v)
+    {
+        super.onClick(v);
+        if (v.getId() == R.id.btnSend)
+        {
+            sendMessage();
+        }
+
+    }
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+
+            int milliseconds = (int) (updatedTime % 1000);
+            if( mins==1){
+                finish();
+            }
+            timerValue.setText("" + mins + ":"
+
+                    + String.format("%02d", secs));
+
+            customHandler.postDelayed(this, 0);
+
+        }
+
+    };
+    /**
+     * Call this method to Send message to opponent. It does nothing if the text
+     * is empty otherwise it creates a Parse object for Chat message and send it
+     * to Parse server.
+     */
+    private void user()
+    {
+        if (txt1.length() == 0)
+            return;
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(txt.getWindowToken(), 0);
+
+        String s = txt.getText().toString();
+        final Conversation c = new Conversation(s, new Date(),
+                UserList.user.getUsername());
+        c.setStatus(Conversation.STATUS_SENDING);
+        convList.add(c);
+        adp.notifyDataSetChanged();
+        txt.setText(null);
+        ParseObject po = new ParseObject("Chat");
+        po.put("sender", UserList.user.getUsername());
+        po.put("receiver", buddy);
+        po.put("special", "special");
+        // po.put("createdAt", "");
+        po.put("message", s);
+        po.saveEventually(new SaveCallback() {
+
+            @Override
+            public void done(ParseException e)
+            {
+                if (e == null)
+                    c.setStatus(Conversation.STATUS_SENT);
+                else
+                    c.setStatus(Conversation.STATUS_FAILED);
+                adp.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    private void sendMessage()
+    {
+        if (txt.length() == 0)
+            return;
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(txt.getWindowToken(), 0);
+
+        String s = txt.getText().toString();
+        final Conversation c = new Conversation(s, new Date(),
+                UserList.user.getUsername());
+        c.setStatus(Conversation.STATUS_SENDING);
+        convList.add(c);
+        adp.notifyDataSetChanged();
+        txt.setText(null);
+
+        ParseObject po = new ParseObject("Chat");
+        po.put("sender", UserList.user.getUsername());
+        po.put("receiver", buddy);
+        // po.put("createdAt", "");
+        po.put("special", "special");
+        s= "Special MESSAGE WILL BE DELETE" +s;
+        po.put("message", s);
+
+        po.saveEventually(new SaveCallback() {
+
+            @Override
+            public void done(ParseException e)
+            {
+                if (e == null)
+                    c.setStatus(Conversation.STATUS_SENT);
+                else
+                    c.setStatus(Conversation.STATUS_FAILED);
+                adp.notifyDataSetChanged();
+            }
+        });
+    }
+
+    /**
+     * Load the conversation list from Parse server and save the date of last
+     * message that will be used to load only recent new messages
+     */
+    private void loadConversationList()
+    {
+        ParseQuery<ParseObject> q = ParseQuery.getQuery("SpecialChat");
+        if (convList.size() == 0)
+        {
+            // load all messages...
+            ArrayList<String> al = new ArrayList<String>();
+            al.add(buddy);
+            al.add(UserList.user.getUsername());
+            q.whereContainedIn("receiver", al);
+        }
+        else
+        {
+            // load only newly received message..
+            if (lastMsgDate != null)
+                q.whereGreaterThan("createdAt", lastMsgDate);
+            q.whereEqualTo("sender", buddy);
+        }
+        q.orderByDescending("createdAt");
+        q.findInBackground(new FindCallback<ParseObject>() {
+
+            @Override
+            public void done(List<ParseObject> li, ParseException e)
+            {
+                if (li != null && li.size() > 0)
+                {
+                    for (int i = li.size() - 1; i >= 0; i--)
+                    {
+
+
+
+                        ParseObject po = li.get(i);
+
+                        Conversation c = new Conversation(po
+                                .getString("message"), po.getCreatedAt(), po
+                                .getString("sender"));
+
+                        convList.add(c);
+                        if (lastMsgDate == null
+                                || lastMsgDate.before(c.getDate()))
+
+                            lastMsgDate = c.getDate();
+                        adp.notifyDataSetChanged();
+                    }
+                }
+                handler.postDelayed(new Runnable() {
+
+                    @Override
+                    public void run()
+                    {
+                        if (isRunning)
+                            loadConversationList();
+                    }
+                }, 1000);
+            }
+        });
+
+    }
+
+    /**
+     * The Class ChatAdapter is the adapter class for Chat ListView. This
+     * adapter shows the Sent or Receieved Chat message in each list item.
+     */
+    private class ChatAdapter extends BaseAdapter
+    {
+
+        /* (non-Javadoc)
+         * @see android.widget.Adapter#getCount()
+         */
+        @Override
+        public int getCount()
+        {
+            return convList.size();
+        }
+
+        /* (non-Javadoc)
+         * @see android.widget.Adapter#getItem(int)
+         */
+        @Override
+        public Conversation getItem(int arg0)
+        {
+            return convList.get(arg0);
+        }
+
+        /* (non-Javadoc)
+         * @see android.widget.Adapter#getItemId(int)
+         */
+        @Override
+        public long getItemId(int arg0)
+        {
+            return arg0;
+        }
+
+        /* (non-Javadoc)
+         * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+         */
+        @Override
+        public View getView(int pos, View v, ViewGroup arg2)
+        {
+            Conversation c = getItem(pos);
+            if (c.isSent())
+                v = getLayoutInflater().inflate(R.layout.chat_item_sent, null);
+            else
+                v = getLayoutInflater().inflate(R.layout.chat_item_rcv, null);
+
+            TextView lbl = (TextView) v.findViewById(R.id.lbl1);
+
+            lbl.setText(DateUtils.getRelativeDateTimeString(SpecialChat.this, c
+                            .getDate().getTime(), DateUtils.SECOND_IN_MILLIS,
+                    DateUtils.DAY_IN_MILLIS, 0));
+
+            lbl = (TextView) v.findViewById(R.id.lbl2);
+
+            lbl.setText(c.getMsg());
+
+            lbl = (TextView) v.findViewById(R.id.lbl3);
+            if (c.isSent())
+            {
+                if (c.getStatus() == Conversation.STATUS_SENT)
+                    lbl.setText("");
+                else if (c.getStatus() == Conversation.STATUS_SENDING)
+                    lbl.setText("Sending...");
+                else
+                    lbl.setText("Failed");
+            }
+            else
+                lbl.setText("");
+
+            return v;
+        }
+
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home)
+        {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
