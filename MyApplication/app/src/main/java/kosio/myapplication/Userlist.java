9b6a4085ed55887a475ca8a11e6ceb55599274f8
jedaitemple <kosio972@mail.bg>
2016-02-03 14:39:35 +0000

    d
    d

---
+++ MyApplication/app/src/main/java/kosio/myapplication/Userlist.java
@@ -0,0 +1,447 @@
+package com.chatt.demo;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
+import android.text.format.DateUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.io.ByteArrayOutputStream;
+
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.graphics.Bitmap;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import com.chatt.demo.custom.CustomActivity;
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+import android.widget.ImageView;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import com.chatt.demo.custom.CustomActivity;
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+import com.chatt.demo.utils.Utils;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
+import android.text.format.DateUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.io.ByteArrayOutputStream;
+
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.graphics.Bitmap;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import com.chatt.demo.custom.CustomActivity;
+import com.chatt.demo.model.Conversation;
+import com.chatt.demo.utils.Const;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+import android.widget.ImageView;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+/**
+ * The Class UserList is the Activity class. It shows a list of all users of
+ * this app. It also shows the Offline/Online status of users.
+ */
+public class UserList extends CustomActivity
+{	private static int RESULT_LOAD_IMAGE = 1;
+	Button logout;
+	/** The Chat list. */
+	private ArrayList<ParseUser> uList;
+	private EditText group;
+	/** The user. */
+	public static ParseUser user;
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_list);
+
+		final Button switchact =(Button)findViewById(R.id.btn1);
+		switchact.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent act2 = new Intent(view.getContext(),group.class);
+				startActivity(act2);
+
+			}
+		});
+		final Button switchact2 =(Button)findViewById(R.id.btn2);
+		switchact2.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent act2 = new Intent(view.getContext(), profile.class);
+				startActivity(act2);
+
+			}
+		});
+		final Button all =(Button)findViewById(R.id.all);
+		all.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent act2 = new Intent(view.getContext(), Statistics.class);
+				startActivity(act2);
+
+			}
+		});
+
+		getActionBar().setDisplayHomeAsUpEnabled(false);
+
+		updateUserStatus(true);
+		TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+		tabHost.setup();
+
+		TabHost.TabSpec tabSpec = tabHost.newTabSpec("Contacts");
+		tabSpec.setContent(R.id.tabContactList);
+		tabSpec.setIndicator("contacts");
+		tabHost.addTab(tabSpec);
+
+
+
+		tabSpec = tabHost.newTabSpec("Groups");
+		tabSpec.setContent(R.id.tabGroups);
+		tabSpec.setIndicator("Groups");
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("opt");
+		tabSpec.setContent(R.id.options);
+		tabSpec.setIndicator("opt");
+		tabHost.addTab(tabSpec);
+
+
+// Locate Button in welcome.xml
+		final Button switchact3 =(Button)findViewById(R.id.special);
+		switchact3.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent act2 = new Intent(view.getContext(),SpecialChat.class);
+				startActivity(act2);
+
+			}
+		});
+
+/*
+
+		logout = (Button) findViewById(R.id.logout);
+
+		// Logout Button Click Listener
+		logout.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View arg0) {
+				// Logout current user
+				ParseUser.logOut();
+				finish();
+			}
+		});*/
+
+	}
+	private void navigateToLogin() {
+		// Launch the login activity
+
+		Intent intent = new Intent(this, Login.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		startActivity(intent);
+	}
+
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		updateUserStatus(false);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onResume()
+	 */
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		loadUserList();
+
+	}
+
+	/**
+	 * Update user status.
+	 *
+	 * @param online
+	 *            true if user is online
+	 */
+	private void updateUserStatus(boolean online)
+	{
+		user.put("online", online);
+		user.saveEventually();
+	}
+
+	/**
+	 * Load list of users.
+	 */
+	private void loadUserList()
+	{
+		final ProgressDialog dia = ProgressDialog.show(this, null,
+				getString(R.string.alert_loading));
+		ParseUser.getQuery().whereNotEqualTo("username", user.getUsername())
+				.findInBackground(new FindCallback<ParseUser>() {
+
+					@Override
+					public void done(List<ParseUser> li, ParseException e) {
+						dia.dismiss();
+						if (li != null) {
+							if (li.size() == 0)
+								Toast.makeText(UserList.this,
+										R.string.msg_no_user_found,
+										Toast.LENGTH_SHORT).show();
+
+							uList = new ArrayList<ParseUser>(li);
+							ListView list = (ListView) findViewById(R.id.list);
+							list.setAdapter(new UserAdapter());
+							list.setOnItemClickListener(new OnItemClickListener() {
+
+								@Override
+								public void onItemClick(AdapterView<?> arg0,
+														View arg1, int pos, long arg3) {
+									startActivity(new Intent(UserList.this,
+											Chat.class).putExtra(
+											Const.EXTRA_DATA, uList.get(pos)
+													.getUsername()));
+								}
+							});
+						} else {
+							Utils.showDialog(
+									UserList.this,
+									getString(R.string.err_users) + " "
+											+ e.getMessage());
+							e.printStackTrace();
+						}
+					}
+				});
+	}
+	/**
+	 * The Class UserAdapter is the adapter class for User ListView. This
+	 * adapter shows the user name and it's only online status for each item.
+	 */
+	private class UserAdapter extends BaseAdapter
+	{
+
+		/* (non-Javadoc)
+		 * @see android.widget.Adapter#getCount()
+		 */
+		@Override
+		public int getCount()
+		{
+			return uList.size();
+		}
+
+		/* (non-Javadoc)
+		 * @see android.widget.Adapter#getItem(int)
+		 */
+		@Override
+		public ParseUser getItem(int arg0)
+		{
+			return uList.get(arg0);
+		}
+
+		/* (non-Javadoc)
+		 * @see android.widget.Adapter#getItemId(int)
+		 */
+		@Override
+		public long getItemId(int arg0)
+		{
+			return arg0;
+		}
+
+		/* (non-Javadoc)
+		 * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
+		 */
+		@Override
+		public View getView(int pos, View v, ViewGroup arg2)
+		{
+			if (v == null)
+				v = getLayoutInflater().inflate(R.layout.chat_item, null);
+
+			ParseUser c = getItem(pos);
+			TextView lbl = (TextView) v;
+			lbl.setText(c.getUsername());
+			lbl.setCompoundDrawablesWithIntrinsicBounds(
+					c.getBoolean("online") ? R.drawable.ic_online
+							: R.drawable.ic_username, 0, R.drawable.arrow, 0);
+			return v;
+		}
+
+	}
+}
