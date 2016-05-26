f302fdaaf72d857972141d449870e849b80ab4c1
jedaitemple <kosio972@mail.bg>
2016-05-26 11:40:13 +0300

    Update Begining.java

--- MyApplication/app/src/main/java/kosio/myapplication/Begining.java
+++ MyApplication/app/src/main/java/kosio/myapplication/Begining.java
@@ -1,40 +1,47 @@
 package kosio.myapplication;
 
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+import com.parse.Parse;
+
 /**
- * Created by Kosioplay on 3.2.2016 г..
+ * The Class Beginning is the Main Application class of this app. The onCreate
+ * method of this class initializes the Parse.
  */
-
-        import android.app.Application;
-        import com.parse.Parse;
-        import com.parse.ParseACL;
-
-        import com.parse.ParseUser;
-
-/**
- * The main class connection to Parse
- */
-public class Begining extends Application
+public class Beginning extends Application
 {
 
-    /* (non-Javadoc)
-     * @see android.app.Application#onCreate()
-     */
-    @Override
-    public void onCreate()
-    {
-        super.onCreate();
+	/* (non-Javadoc)
+	 * @see android.app.Application#onCreate()
+	 */
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
 
-        Parse.initialize(this, "nE2rM3YtEq45TU6ktVBTzekMIFilQzg5c693IueV",
-                "v3NTJ6khp6X2q7uqU7fNhFMsWfMiMnuWjuigz6KQ");
-        ParseUser.enableAutomaticUser();
-        ParseACL defaultACL = new ParseACL();
+		Parse.initialize(this, "nE2rM3YtEq45TU6ktVBTzekMIFilQzg5c693IueV",
+				"v3NTJ6khp6X2q7uqU7fNhFMsWfMiMnuWjuigz6KQ");
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
 
+		// If you would like all objects to be private by default, remove this
+		// line.
+		defaultACL.setPublicReadAccess(true);
 
-        defaultACL.setPublicReadAccess(true);
+		Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
 
-        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+		ParseUser.enableAutomaticUser();
 
+		// If you would like all objects to be private by default, remove this
+		// line.
+		defaultACL.setPublicReadAccess(true);
 
+		ParseACL.setDefaultACL(defaultACL, true);
 
-    }
+	}
 }
